d2e078095e8e70a424da2bea9433815219729385
Lukasz Piasny <lukaspiasny@gmail.com>
2025-05-08 16:58:35 +0200

    Feature/flas 3 user (#1)
    * FLAS-14 Add user entity, repo and security
    
    * FLAS-15 Add login form
    
    * FLAS-15 Add styles
    
    * FLAS-15 Add bootstrap lib
    
    * FLAS-15 Display header and footer
    
    * FLAS-15 Move login form into homepage
    
    * FLAS-15 Add HomeControllerTest
    
    * FLAS-15 Set translations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,15 +9,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
+     * @param \Symfony\Component\Security\Http\Authentication\AuthenticationUtils $authenticationUtils
      * @return \Symfony\Component\HttpFoundation\Response
      */
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('pages/home.html.twig');
+        if($this->getUser()) {
+            return $this->redirectToRoute('app_panel');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('pages/home.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 }
