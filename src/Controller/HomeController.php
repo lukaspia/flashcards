5573fa90d8aa0729c47550d13e7851c698fabd7f
lukasz <lukaspiasny@gmail.com>
2025-05-08 12:28:26 +0200

    FLAS-15 Move login form into homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
@@ -16,8 +17,19 @@ class HomeController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      */
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('pages/home.html.twig');
+        if($this->getUser()) {
+            return $this->redirectToRoute('app_panel');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('pages/home.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 }
