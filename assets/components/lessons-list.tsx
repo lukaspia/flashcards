1bf8124a70d98cd80b4e689a3ab158ad0b6e2774
lukasz <lukaspiasny@gmail.com>
2025-05-13 17:42:13 +0200

    FLAS-25 Add dialog component

--- assets/components/lessons-list.tsx
+++ assets/components/lessons-list.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import Button from "./button";
+import Button from '@mui/material/Button';
+import FormDialog from './dialog';
 
 export default function LessonsList(): React.ReactElement {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className="lesson-list">
             <h1>Lista lekcji</h1>
-            <Button title={"Dodaj lekcję"} type={"primary"} clickHandler={() => {console.log("Dodaj lekcję")}} />
+            <Button className="btn btn-primary" variant="contained" onClick={handleClickOpen}>Dodaj lekcję</Button>
+            <FormDialog open={open} handleClose={handleClose} />
         </div>
     );
 };
