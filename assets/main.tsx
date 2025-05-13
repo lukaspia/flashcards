ffbc8e28c955b0612cc2534623991c8ebc44d35f
lukasz <lukaspiasny@gmail.com>
2025-05-13 16:16:08 +0200

    FLAS-25 Add init function

--- assets/main.tsx
+++ assets/main.tsx
@@ -2,24 +2,35 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import {BrowserRouter as Router, Routes, Route} from "react-router";
 
+const NotFound = () => <h1>Page Not Found</h1>;
 
 function Main() {
     return(
         <Router>
             <Routes>
                 <Route path="/panel" element={<h1>Home</h1>} />
+                <Route path="/*" element={<NotFound />} />
             </Routes>
         </Router>
     );
 }
 
 
-const rootElement = document.getElementById("root");
+function initApp() {
+    const rootElement = document.getElementById("root");
 
-const root = createRoot(rootElement!);
+    if (!rootElement) {
+        console.error("Root element not found.");
+        return;
+    }
 
-root.render(
-    <StrictMode>
-        <Main />
-    </StrictMode>
-);
+    const root = createRoot(rootElement);
+
+    root.render(
+        <StrictMode>
+            <Main />
+        </StrictMode>
+    );
+}
+
+initApp();
