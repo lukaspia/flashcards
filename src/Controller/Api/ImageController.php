e95c9bedd1f34a5742d0048ec5173b812211a552
lukasz <lukaspiasny@gmail.com>
2025-06-27 15:14:36 +0200

    FLAS-44 Add loggers

--- src/Controller/Api/ImageController.php
+++ src/Controller/Api/ImageController.php
@@ -12,6 +12,8 @@ use App\File\FileNameGeneratorInterface;
 use App\Form\UploadWordImageTypeForm;
 use App\Service\Lesson\WordImageServiceInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,7 +25,8 @@ class ImageController extends AbstractApiController
         EntityManagerInterface $entityManager,
         private readonly WordImageServiceInterface $wordServices,
         private readonly WordImageProcessorFactoryInterface $wordImageProcessorFactory,
-        public readonly FileNameGeneratorInterface $fileNameGenerator
+        private readonly FileNameGeneratorInterface $fileNameGenerator,
+        private readonly LoggerInterface $logger
     ) {
         parent::__construct($entityManager);
     }
@@ -41,33 +44,44 @@ class ImageController extends AbstractApiController
 
         $form = $this->createForm(UploadWordImageTypeForm::class);
         $form->handleRequest($request);
-        $errors = $form->getErrors(true);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $imageFile = $form->get('image')->getData();
-            $wordId = $form->get('word')->getData();
-
-            if ($imageFile) {
-                $newFilename = $this->fileNameGenerator->generate((string)$wordId, $imageFile->getClientOriginalName());
-
-                try {
-                    $processor = $this->wordImageProcessorFactory->createProcessor((int)$wordId);
-                    $image = $processor->process($imageFile, $newFilename);
-
-                    return $this->createResponse(['image' => $image, 'url' => ''],
-                                                 ['Image uploaded successfully'],
-                                                 Response::HTTP_OK);
-                } catch (FileException $e) {
-                    return $this->createResponse(
-                        null,
-                        ['Upload image error: ' . $e->getMessage()],
-                        Response::HTTP_INTERNAL_SERVER_ERROR
-                    );
-                }
-            }
+        if(!$form->isSubmitted() || !$form->isValid()) {
+            $errors = $form->getErrors(true);
+            $this->logger->warning('Image upload validation failed', [
+                'errors' => $errors,
+                'user' => $this->getUser()->getId()
+            ]);
+            return $this->createResponse($errors, ['Image upload validation failed'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
-        return $this->createResponse($errors, ['Something went wrong'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        $imageFile = $form->get('image')->getData();
+        $wordId = $form->get('word')->getData();
+
+        if (!$imageFile instanceof UploadedFile) {
+            return $this->createResponse(null, ['Please upload a file'], Response::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            $newFilename = $this->fileNameGenerator->generate((string)$wordId, $imageFile->getClientOriginalName());
+            $processor = $this->wordImageProcessorFactory->createProcessor((int)$wordId);
+            $image = $processor->process($imageFile, $newFilename);
+
+            $this->logger->info('Image uploaded successfully', [
+                'wordId' => $wordId,
+                'filename' => $newFilename,
+                'userId' => $this->getUser()->getId()
+            ]);
+
+            return $this->createResponse(['image' => $image, 'url' => ''],
+                                         ['Image uploaded successfully'],
+                                         Response::HTTP_OK);
+        } catch (FileException $e) {
+            return $this->createResponse(
+                null,
+                ['Upload image error: ' . $e->getMessage()],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     /**
