aaeadaaf1970f3b7a5ac3b1dcb8badb516aba946
lukasz <lukaspiasny@gmail.com>
2026-02-24 11:47:28 +0100

    refactor(lesson): cleanup LessonService and add transaction handling

--- src/Service/Lesson/LessonService.php
+++ src/Service/Lesson/LessonService.php
@@ -9,8 +9,9 @@ namespace App\Service\Lesson;
 use App\Entity\Lesson;
 use App\Event\AddLessonEvent;
 use App\Entity\User;
+use App\Event\RemoveLessonEvent;
+use App\Repository\LessonRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Validator\Exception\InvalidArgumentException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
@@ -18,12 +19,12 @@ readonly class LessonService implements LessonServiceInterface
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private LessonRepository $lessonRepository,
         private ValidatorInterface $validator,
         private EventDispatcherInterface $eventDispatcher,
         private WordImageServiceInterface $wordImageServices
     ) {
     }
-
 
     /**
      * @param \App\Entity\Lesson $lesson
@@ -31,7 +32,15 @@ readonly class LessonService implements LessonServiceInterface
      */
     public function addLesson(Lesson $lesson): Lesson
     {
-        $lesson = $this->saveLesson($lesson);
+        $this->validate($lesson);
+
+        $this->entityManager->wrapInTransaction(function () use ($lesson) {
+            $this->entityManager->persist($lesson);
+            $this->entityManager->flush();
+
+            $this->wordImageServices->moveWordsImagesFromTemporary($lesson->getWords());
+        });
+
         $this->eventDispatcher->dispatch(new AddLessonEvent($lesson), AddLessonEvent::NAME);
 
         return $lesson;
@@ -43,7 +52,14 @@ readonly class LessonService implements LessonServiceInterface
      */
     public function updateLesson(Lesson $lesson): Lesson
     {
-        return $this->saveLesson($lesson);
+        $this->validate($lesson);
+
+        $this->entityManager->wrapInTransaction(function () use ($lesson) {
+            $this->entityManager->flush();
+            $this->wordImageServices->moveWordsImagesFromTemporary($lesson->getWords());
+        });
+
+        return $lesson;
     }
 
     /**
@@ -52,51 +68,22 @@ readonly class LessonService implements LessonServiceInterface
      */
     public function removeLesson(Lesson $lesson): Lesson
     {
-        $this->entityManager->remove($lesson);
-        $this->entityManager->flush();
+        $this->entityManager->wrapInTransaction(function () use ($lesson) {
+            $this->entityManager->remove($lesson);
+            $this->entityManager->flush();
+        });
 
-        $this->eventDispatcher->dispatch(new AddLessonEvent($lesson), AddLessonEvent::NAME);
-
-        return $lesson;
-    }
-
-    /**
-     * @param \App\Entity\Lesson $lesson
-     * @return \App\Entity\Lesson
-     */
-    private function saveLesson(Lesson $lesson): Lesson
-    {
-        $errors = $this->validator->validate($lesson);
-
-        if (count($errors) > 0) {
-            throw new InvalidArgumentException((string)$errors);
-        }
-
-        $this->entityManager->persist($lesson);
-        $this->entityManager->flush();
-
-        $this->wordImageServices->moveWordsImagesFromTemporary($lesson->getWords());
+        $this->eventDispatcher->dispatch(new RemoveLessonEvent($lesson), RemoveLessonEvent::NAME);
 
         return $lesson;
     }
 
     /**
-     * @param \App\Entity\User $user
-     * @param int $page
-     * @param int $limit
-     * @return array{
-     *   lessons: \App\Entity\Lesson[],
-     *   page: int,
-     *   totalItems: int,
-     *   totalPages: int
-     * }
+     * @return array{lessons: Lesson[], page: int, totalItems: int, totalPages: int}
      */
     public function getUserLessonsWithPagination(User $user, int $page, int $limit): array
     {
-        /** @var \App\Repository\LessonRepository $lessonRepository */
-        $lessonRepository = $this->entityManager->getRepository(Lesson::class);
-
-        $paginator = $lessonRepository->getPaginatedLessons(
+        $paginator = $this->lessonRepository->getPaginatedLessons(
             ['user' => $user],
             ['id' => 'DESC'],
             $limit,
@@ -104,15 +91,27 @@ readonly class LessonService implements LessonServiceInterface
         );
 
         $totalItems = count($paginator);
-        $totalPages = (int) ceil($totalItems / $limit);
-
-        $page = $totalPages > 0 ? max(1, min($page, $totalPages)) : 1;
+        $totalPages = (int)ceil($totalItems / $limit);
+        $safePage = $totalPages > 0 ? max(1, min($page, $totalPages)) : 1;
 
         return [
             'lessons' => iterator_to_array($paginator),
-            'page' => $page,
+            'page' => $safePage,
             'totalItems' => $totalItems,
             'totalPages' => $totalPages
         ];
+    }
+
+    /**
+     * @param \App\Entity\Lesson $lesson
+     * @return void
+     */
+    private function validate(Lesson $lesson): void
+    {
+        $errors = $this->validator->validate($lesson);
+
+        if (count($errors) > 0) {
+            throw new \InvalidArgumentException((string)$errors);
+        }
     }
 }
